f59d1893e6427fb71bebab223958cc5104f76ef0
ldias89 <lucasdias@haf.com.br>
2025-07-10 08:49:49 -0300

    feat: frontend part 2

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Task Manager</h1>
+    </main>
+  );
+}
